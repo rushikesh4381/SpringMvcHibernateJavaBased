91ea206dab60110b846bdb201db6c0aaec058533
rushikesh <admin@0835-2509>
2017-12-28 17:10:48 +0530

    spring test cases added ...

--- SpringMvcHibernateJavaBased/src/test/java/net/codejava/spring/AdditionTestCase.java
+++ SpringMvcHibernateJavaBased/src/test/java/net/codejava/spring/AdditionTestCase.java
@@ -1,21 +1,67 @@
 package net.codejava.spring;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
 
 import org.junit.After;
-import org.junit.Assert;
+
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
+import net.codejava.spring.config.ApplicationContextConfig;
+import net.codejava.spring.dao.UserDAO;
+import net.codejava.spring.model.User;
 import net.test.Addition;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = ApplicationContextConfig.class)
+@WebAppConfiguration
 public class AdditionTestCase {
-
+	
+	@Autowired
+	UserDAO userDAO;
+	
 	@Test
 	public void test() {
-		Addition addTestCase = new Addition();
+		
+		/*Addition addTestCase = new Addition();
 		int add = addTestCase.add(10, 20);
-		Assert.assertEquals(30, add);
+		Assert.assertEquals(30, add);*/
+		
+	//	System.out.println("sessionfactory :"+sessionFactory);
+		System.out.println("user dao object :"+userDAO);
+		
+		User user = new User();
+		user.setUsername("Alex123");
+		user.setPassword("alex3222");
+		user.setEmail("alex@gmail.com");
+		
+		userDAO.saveOrUpdate(user);
+		List<User> list =userDAO.list();
+		String actualUserName = null;
+		String actualPassword = null;
+		String actualEmail = null;
+		for(User u : list)
+		{
+			actualUserName = u.getUsername();
+			actualEmail = u.getEmail();
+			actualPassword = u.getPassword();
+			
+			if(user.getUsername().equalsIgnoreCase(actualUserName) && user.getPassword().equalsIgnoreCase(actualPassword)
+					&& user.getEmail().equalsIgnoreCase(actualEmail))
+			{
+				break;
+			}
+		}
+		assertEquals(user.getUsername(),actualUserName);
+		assertEquals(user.getPassword(), actualPassword);
+		assertEquals(user.getEmail(), actualEmail);
 	}
 	
 	@Before
